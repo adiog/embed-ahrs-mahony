31dbf297aea8d4180e9f4e9d59657edf4f76f237
adiog <adiog@brainfuck.pl>
2018-05-01 12:23:13 +0200

    clang-format

--- src/MahonyAHRS.h
+++ src/MahonyAHRS.h
@@ -16,9 +16,9 @@
 //----------------------------------------------------------------------------------------------------
 // Variable declaration
 
-extern volatile float twoKp;			// 2 * proportional gain (Kp)
-extern volatile float twoKi;			// 2 * integral gain (Ki)
-extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
+extern volatile float twoKp;           // 2 * proportional gain (Kp)
+extern volatile float twoKi;           // 2 * integral gain (Ki)
+extern volatile float q0, q1, q2, q3;  // quaternion of sensor frame relative to auxiliary frame
 
 //---------------------------------------------------------------------------------------------------
 // Function declarations
